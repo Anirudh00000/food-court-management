cc2da651d50c9ec4c4bd7141c64434adbb597925
ANIRUDH SINGH <anirudh8400@gmail.com>
2022-06-20 09:13:15 +0530

    first commit

---
+++ foodcourt_management/foodcourt_management/commonfeild.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace foodcourt_management
+{
+    internal class commonfeild
+    {
+    }
+}
